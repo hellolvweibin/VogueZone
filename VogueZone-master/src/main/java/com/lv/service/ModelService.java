22304a46a035f971b157a120e8cab53603ebdc1c
Levi_Bee <279369954@qq.com>
2022-04-08 20:54:33 +0800

    第五次提交

---
+++ VogueZone-master/src/main/java/com/lv/service/ModelService.java
@@ -0,0 +1,16 @@
+package com.lv.service;
+
+import com.lv.entity.Model;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author levi_bee
+ * @since 2022-04-08
+ */
+public interface ModelService extends IService<Model> {
+
+}
